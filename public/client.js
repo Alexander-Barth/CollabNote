ce4474a7e4ab6ba1bae9c5d4dca32e34413fbc93
Alexander Barth <barth.alexander@gmail.com>
2016-10-01 23:08:10 +0200

    initial file upload

--- public/client.js
+++ public/client.js
@@ -337,6 +337,67 @@ function init() {
         socket = null;
     }
 
+
+    // media upload
+    var form = document.getElementById('file-form');
+    var fileSelect = document.getElementById('file-select');
+    var uploadButton = document.getElementById('upload-button');
+
+    form.onsubmit = function(event) {
+        event.preventDefault();
+
+        // Update button text.
+        uploadButton.innerHTML = 'Uploading...';
+        
+        // The rest of the code will go here...
+        // Get the selected files from the input.
+        var files = fileSelect.files;
+        // Create a new FormData object.
+        var formData = new FormData();
+
+        // Loop through each of the selected files.
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+
+            // Check the file type.
+            if (!file.type.match('image.*')) {
+                continue;
+            }
+
+            // Add the file to the request.
+            formData.append('media', file, file.name);
+        }
+
+
+        // Set up the request.
+        var xhr = new XMLHttpRequest();
+
+        // Open the connection.
+        xhr.open('POST', '/upload', true);
+        // Set up a handler for when the request finishes.
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                // File(s) uploaded.
+                uploadButton.innerHTML = 'Upload';
+            } else {
+                alert('An error occurred!');
+            }
+        };
+        //Call a function when the state changes.
+
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4 && xhr.status == 200) {
+                console.log(xhr.responseText);
+                var data = JSON.parse(xhr.responseText);
+                set_document(doc.value + '![alt text](media/' + data.mediaid + ' "title")');
+            }
+        }
+
+        // Send the Data.
+        xhr.send(formData);
+
+    }
+
     test();
 };
 
--- server.js
+++ server.js
@@ -2,17 +2,20 @@
 // License: AGPL v3, https://www.gnu.org/licenses/agpl-3.0.txt
 // Copyright (C) 2016 Alexander Barth, https://github.com/Alexander-Barth
 
-var WebSocketServer = require('ws').Server;
-var http = require('http');
+var express = require('express');
+var multer  = require('multer')
+var upload = multer();
+
+var app = express();
+var expressWs = require('express-ws')(app);
 
 var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8000
 
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
-
 // mongodb
-var MongoClient = require('mongodb').MongoClient;
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var Grid = mongodb.Grid;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/collabnote';
@@ -26,31 +29,104 @@ if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_APP_NAME;
 }
 
-// Serve up public folder
-var serve = serveStatic('public/', {'index': ['index.html']});
+function makeid(len)
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-var server = http.createServer(function(req, res) {
-    var done = finalhandler(req, res);
-    serve(req, res, done);
+    for( var i=0; i < len; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+app.use(express.static('public'));
+ 
+// app.use(function (req, res, next) {
+//   console.log('middleware');
+//   req.testing = 'testing';
+//   return next();
+// });
+ 
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
 });
 
-var  wss = new WebSocketServer({server:server});
+app.param('id', function (req, res, next, id) {
+  console.log('CALLED ONLY ONCE');
+  next();
+});
+
+app.get('/media/:id', function(req, res) {
+    console.log('get route', req.params.id);
+    
+    var mediaid = req.params.id;
+    
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        db.collection('media').findOne(
+            {'mediaid': mediaid},
+            function(err, doc) {
+                console.log(typeof doc.media);
+                console.log('check ',doc.media instanceof Buffer,Object.prototype.toString.call(doc.media));
+                console.log('doc.media',doc.media);
+                res.send(doc.media.buffer);
+                res.end();
+                db.close();
+            });            
+    });
+});
+
+
+
+app.post('/upload', upload.single('media'), function(req, res) {
+    var sampleFile;
+
+    // check if file is an image or video, otherwise reject
+    console.log('req',req);
+    console.log('req.file',req.file);
+    // make sure mediaid is unique
+    var mediaid = makeid(5) + '-' + req.file.originalname;
+
+    var buffer = req.file.buffer;
+
+    console.log('buffer',buffer);
+    console.log('buffer data',buffer instanceof Buffer);
+
+    var media = new mongodb.Binary(buffer);
+    console.log('media',media);
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+
+        db.collection('media').insertOne(
+            {'mediaid': mediaid,
+             'media': media},
+            function(err,result) {
+                assert.equal(err, null);
+                console.log("Inserted data into",mediaid);
+                db.close();
+            });
+
+    });
+
+
+    res.send(JSON.stringify({'mediaid': mediaid}));
+}); 
 
 // all connections
 connections = [];
 
-wss.on('connection', function(ws) {
+app.ws('/', function(ws, req) {
     console.log('Client connected');
     // add to list of active connections
     connections.push(ws);
 
-    ws.on('message', function(msg, flags) {
-        if (flags.binary) { 
-            return; 
-        }
-        
+
+    ws.on('message', function(msg) {
         var data = JSON.parse(msg);
-        //console.log('got ' + msg);
+        console.log('got ' + msg);
 
         if (data.command === 'subscribe') {
             ws.docid = data.docid;
@@ -123,9 +199,8 @@ wss.on('connection', function(ws) {
     ws.on('error', function(e) {
       console.log(e);
     });
-});
 
-// Listen
-server.listen(port, ipaddr, function() {
-    console.log((new Date()) + ': Listening at http://' + ipaddr + ':' + port);
+    console.log('socket', req.testing);
 });
+ 
+app.listen(8000);
