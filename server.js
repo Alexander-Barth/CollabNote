94333fec1956605f81a69a3077806ad73ac63ee3
Alexander Barth <barth.alexander@gmail.com>
2016-10-02 22:31:08 +0200

    add config.js

--- server.js
+++ server.js
@@ -2,15 +2,18 @@
 // License: AGPL v3, https://www.gnu.org/licenses/agpl-3.0.txt
 // Copyright (C) 2016 Alexander Barth, https://github.com/Alexander-Barth
 
+var config  = require('./config')
 var express = require('express');
+// for file upload
 var multer  = require('multer')
 var upload = multer();
 
+// get mime type by file extension
+var mime = require('mime');
+
+
 var app = express();
 var expressWs = require('express-ws')(app);
-
-var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8000
 
 // mongodb
 var mongodb = require('mongodb');
@@ -18,16 +21,6 @@ var MongoClient = mongodb.MongoClient;
 var Grid = mongodb.Grid;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/collabnote';
-
-// if OPENSHIFT env variables are present, use the available connection info:
-if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-  url = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
 
 function makeid(len)
 {
@@ -41,37 +34,21 @@ function makeid(len)
 }
 
 app.use(express.static('public'));
- 
-// app.use(function (req, res, next) {
-//   console.log('middleware');
-//   req.testing = 'testing';
-//   return next();
-// });
- 
-app.get('/', function(req, res, next){
-  console.log('get route', req.testing);
-  res.end();
-});
 
 app.param('id', function (req, res, next, id) {
-  console.log('CALLED ONLY ONCE');
   next();
 });
 
 app.get('/media/:id', function(req, res) {
-    console.log('get route', req.params.id);
+    //console.log('get route', req.params.id);
     
     var mediaid = req.params.id;
     
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(config.db.url, function(err, db) {
         assert.equal(null, err);
         db.collection('media').findOne(
             {'mediaid': mediaid},
             function(err, doc) {
-                console.log(typeof doc.media);
-                console.log('check ',doc.media instanceof Buffer,Object.prototype.toString.call(doc.media));
-                console.log('doc.media',doc.media);
-
                 res.setHeader('Content-Type', doc.mimetype);
                 res.send(doc.media.buffer);
                 res.end();
@@ -83,11 +60,8 @@ app.get('/media/:id', function(req, res) {
 
 
 app.post('/upload', upload.single('media'), function(req, res) {
-    var sampleFile;
-
-    // check if file is an image or video, otherwise reject
-    console.log('req',req);
-    console.log('req.file',req.file);
+    //console.log('req',req);
+    //console.log('req.file',req.file);
 
     if (!req.file) {
         res.status(400).send({
@@ -96,9 +70,13 @@ app.post('/upload', upload.single('media'), function(req, res) {
         return;
     }
 
-    var accepted_mimetypes = ['image/png','image/jpeg','image/gif','image/svg+xml'];
+    // check if file is an image or video, otherwise reject
+    // by checking the mimetype and the file extension
     var mimetype = req.file.mimetype;
-    if (accepted_mimetypes.indexOf(mimetype) === -1) {
+    var mimetype_ext = mime.lookup(req.file.originalname);
+
+    if (config.accepted_mimetypes.indexOf(mimetype) === -1 ||
+        config.accepted_mimetypes.indexOf(mimetype_ext) === -1) {
         // reject
         res.status(415).send({
             message: 'Mimetype "' + req.file.mimetype + '" is not acceptable. I accepted only: ' + accepted_mimetypes.join(', ')
@@ -106,19 +84,13 @@ app.post('/upload', upload.single('media'), function(req, res) {
         return;
     }
 
-
-    // make sure mediaid is unique
+    // TODO: make sure mediaid is unique
     var mediaid = makeid(5) + '-' + req.file.originalname;
 
     var buffer = req.file.buffer;
-
-    console.log('buffer',buffer);
-    console.log('buffer data',buffer instanceof Buffer);
-
     var media = new mongodb.Binary(buffer);
-    console.log('media',media);
 
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(config.db.url, function(err, db) {
         assert.equal(null, err);
 
         db.collection('media').insertOne(
@@ -130,14 +102,13 @@ app.post('/upload', upload.single('media'), function(req, res) {
                 console.log("Inserted data into",mediaid);
                 db.close();
             });
-
     });
 
 
     res.send(JSON.stringify({'mediaid': mediaid}));
 }); 
 
-// all connections
+// list of all connections
 connections = [];
 
 app.ws('/', function(ws, req) {
@@ -148,7 +119,7 @@ app.ws('/', function(ws, req) {
 
     ws.on('message', function(msg) {
         var data = JSON.parse(msg);
-        console.log('got ' + msg);
+        //console.log('got ' + msg);
 
         if (data.command === 'subscribe') {
             ws.docid = data.docid;
@@ -161,7 +132,7 @@ app.ws('/', function(ws, req) {
                 'diff': []
             };
 
-            MongoClient.connect(url, function(err, db) {
+            MongoClient.connect(config.db.url, function(err, db) {
                 assert.equal(null, err);
                 var cursor = db.collection('documents').find({'docid': ws.docid});
 
@@ -181,7 +152,7 @@ app.ws('/', function(ws, req) {
         }
         else if (data.command === 'edit') {
 
-            MongoClient.connect(url, function(err, db) {
+            MongoClient.connect(config.db.url, function(err, db) {
                 assert.equal(null, err);
                 db.collection('documents').insertOne(
                     {'docid': ws.docid,
@@ -221,8 +192,6 @@ app.ws('/', function(ws, req) {
     ws.on('error', function(e) {
       console.log(e);
     });
-
-    console.log('socket', req.testing);
 });
  
-app.listen(8000);
+app.listen(config.port,config.ipaddr);
